85b7d2c7a27a820ed5f6fd083cfe9298c22b5290
vamanpatel <vamanpatel1@gmail.com>
2023-08-12 17:50:50 +0530

    project3

---
+++ project_3/src/main/java/in/co/rays/project_3/dto/DropdownList.java
@@ -0,0 +1,15 @@
+package in.co.rays.project_3.dto;
+
+/**
+ * DropdownList interface is implemented by Beans or dtos those are used to create dropdown 
+ * list on HTML pages
+ * 
+ * @author Vaman Patel
+ *
+ */
+
+public interface DropdownList {
+	public String getKey();
+
+	public String getValue();
+}
